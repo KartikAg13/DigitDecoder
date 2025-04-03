09bcc0ca66faf73ea12ae0d48b51b34876b90411
KartikAg13 <23803013@mail.jiit.ac.in>
2025-04-03 20:33:46 +0530

    lets go

---
+++ main.py
@@ -0,0 +1,5 @@
+from data.mnist_loader import load_mnist
+from evaluate.evaluator import evaluate_model
+from model.mlp import build_model
+from train.trainer import train_model
+from utils.helpers import plot_predictions
