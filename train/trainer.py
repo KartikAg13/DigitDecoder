377428428a0cd475567622f6a0d4200931dfd00c
KartikAg13 <23803013@mail.jiit.ac.in>
2025-04-11 23:27:51 +0530

    add types

--- train/trainer.py
+++ train/trainer.py
@@ -4,14 +4,14 @@ import numpy as np
 def loss_function(y_onehot, A2) -> float:
 	return -np.mean(np.sum(y_onehot * np.log(A2 + 1e-10), axis=1))
 
-def train_model(model: NeuralNetwork, epochs: int, learning_rate: float, x_train, y_train):
+def train_model(model: NeuralNetwork, epochs: int, learning_rate: float, x_train: np.ndarray, y_train: np.ndarray) -> np.ndarray:
 	y_onehot = np.eye(10)[y_train]
 	for epoch in range(epochs):
-		A2 = model.forward_propogation(x_train)
+		A2 = model.forward_propagation(x_train)
 		
 		loss: float = loss_function(y_onehot, A2)
 
-		model.backward_propogation(x_train, y_onehot)
+		model.backward_propagation(x_train, y_onehot)
 		model.update_parameters(learning_rate)
 
 		if epoch % 50 == 0:
